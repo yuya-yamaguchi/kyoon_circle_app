8b1b4f6ddbe0205e2a5ee50338c634a21dae0db1
yuya-yamaguchi <nsy1023n@gmail.com>
2021-01-13 05:16:11 +0900

    fontawesomeの導入

--- front/src/main.js
+++ front/src/main.js
@@ -2,6 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { FontAwesomeIcon } from "@/plugins/font-awesome";
 
 var app = createApp(App);
-app.use(store).use(router).mount('#app');
+app
+.use(store)
+.use(router)
+.component("fa", FontAwesomeIcon)
+.mount('#app');
