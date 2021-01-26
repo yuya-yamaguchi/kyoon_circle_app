ac83aae3d6899077f95d3166923f57bdd63e994f
yuya-yamaguchi <nsy1023n@gmail.com>
2021-01-26 23:49:03 +0900

    トップページのコンポーネント化

---
+++ front/src/components/molecules/top/AboutNews.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="about-news">
+    <p class="top-each-title">NEWS</p>
+    <Loading v-if="loadingProp"/>
+    <NewsList v-if="newsProp.length!=0" :news-prop="newsProp"/>
+    <router-link to="/news?page=1" class="all-view-link">すべてのお知らせを見る</router-link>
+  </div>
+</template>
+
+<script>
+import NewsList from '@/components/NewsList.vue';
+import Loading from '@/components/Loading.vue';
+
+export default {
+  props: {
+    newsProp: {},
+    loadingProp: {}
+  },
+  components: {
+    NewsList,
+    Loading
+  }
+}
+</script>
+
+<style scoped>
+.about-news {
+  padding: 100px 0px;
+  background: #FFF;
+}
+.all-view-link {
+  display: block;
+  text-decoration: none;
+  margin: 0 auto;
+  text-align: center;
+  color: #333;
+  font-size: 20px;
+  font-weight: bold;
+}
+.top-each-title {
+  font-size: 40px;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
---
+++ front/src/components/molecules/top/TopImage.vue
@@ -0,0 +1,28 @@
+<template>
+  <transition name="fade-slow" appear>
+    <div class="top-image-container">
+      <img src="/studio/studio7.jpg">
+      <h1 class="top-page-title">Kyo-ON</h1>
+    </div>
+  </transition>
+</template>
+
+<style scoped lang="scss">
+.top-image-container {
+  position: relative;
+  img {
+    width: 100%;
+    opacity: 0.6;
+    vertical-align: bottom;
+    filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.6));
+  }
+  .top-page-title {
+    font-size: 50px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    opacity: 0.8;
+  }
+}
+</style>
