e287d7174bd6487403501170ed1e93451b80b19f
yuya-yamaguchi <nsy1023n@gmail.com>
2021-04-02 23:38:28 +0900

    イベントエントリーの二重送信防止

--- front/src/mixins/events/eventEntry.js
+++ front/src/mixins/events/eventEntry.js
@@ -2,33 +2,44 @@ import axios from 'axios';
 import g from "@/variable/variable.js";
 
 export const eventEntry = {
+  data() {
+    return {
+      isEnteringEvent: false
+    }
+  },
   methods: {
     postEventEntry: function() {
-      axios.post(
-        `http://${g.hostName}/api/events/${this.$route.params.id}/entry`,
-        {
-          user_id: this.$store.getters['user/id']
-        },
-        {
-          headers: {
-            Authorization: this.$store.getters['user/secureToken']
-          }
-        }
-      )
-      .then((response) => {
-        this.$emit('update-entry-status', true, response.data.entry_users)
-      })
-      .catch((error) => {
-        if (error.response.status === 400) {
-          this.$store.dispatch(
-            "flash/create",
-            { message: error.response.data.error_message,
-              type:    2
+      if (!this.isEnteringEvent) {
+        this.isEnteringEvent = true
+        axios.post(
+          `http://${g.hostName}/api/events/${this.$route.params.id}/entry`,
+          {
+            user_id: this.$store.getters['user/id']
+          },
+          {
+            headers: {
+              Authorization: this.$store.getters['user/secureToken']
             }
-          );
-        }
-        this.apiErrors(error.response);
-      });
+          }
+        )
+        .then((response) => {
+          this.$emit('update-entry-status', true, response.data.entry_users)
+        })
+        .catch((error) => {
+          if (error.response.status === 400) {
+            this.$store.dispatch(
+              "flash/create",
+              { message: error.response.data.error_message,
+                type:    2
+              }
+            );
+          }
+          this.apiErrors(error.response);
+        })
+        .finally(() => {
+          this.isEnteringEvent = false
+        });
+      }
     },
   }
 }
